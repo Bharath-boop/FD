fd3b4aad3f52fc1e415c09f6bdeb9b6df0bfc009
Bharath kumar <bharathboopathi51550@gmail.com>
2024-05-16 19:29:41 +0530

    cahbe

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -1,5 +1,5 @@
 // const BASE_URL="http://localhost:8000/user"
-const BASE_URL="https://server-koj1.onrender.com/user"
+const BASE_URL="https://server-koj1.onrender.com"
 
 export default{
     BASE_URL
